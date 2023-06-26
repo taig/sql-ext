170c91999b9646200f8b31fd113fd3a804cc47a1
Niklas Klein <mail@taig.io>
2023-06-26 16:58:58 +0200

    Add Transaction.use helper

--- src/main/scala/io/taig/skunk/ext/Transaction.scala
+++ src/main/scala/io/taig/skunk/ext/Transaction.scala
@@ -1,7 +1,7 @@
 package io.taig.skunk.ext
 
 import cats.effect.{IO, Resource}
-import skunk.Session
+import skunk.{Session, Transaction}
 
 final class Transaction private (val session: Session[IO], val underlying: skunk.Transaction[IO])
     extends skunk.Transaction[IO]:
@@ -10,3 +10,4 @@ final class Transaction private (val session: Session[IO], val underlying: skunk
 object Transaction:
   def from(session: Session[IO]): Resource[IO, Transaction] = session.transaction.map(new Transaction(session, _))
   def from(sessions: Resource[IO, Session[IO]]): Resource[IO, Transaction] = sessions.flatMap(from)
+  def use[A](sessions: Resource[IO, Session[IO]])(f: Transaction => IO[A]): IO[A] = from(sessions).use(f)
