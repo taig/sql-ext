0fb912d6549907d05bd1828f63488b7ce5a961f2
Niklas Klein <mail@taig.io>
2023-07-06 15:19:57 +0200

    Add Table.apply helper to build a Column

--- src/main/scala/io/taig/skunk/ext/Table.scala
+++ src/main/scala/io/taig/skunk/ext/Table.scala
@@ -1,5 +1,6 @@
 package io.taig.skunk.ext
 
+import cats.syntax.all.*
 import skunk.util.Origin
 import skunk.{Fragment, Void}
 
@@ -7,6 +8,7 @@ opaque type Table = String
 
 object Table:
   extension (self: Table)
+    def apply(column: String): Column = Column(self.some, column)
     def sql(using origin: Origin): Fragment[Void] = Fragment(List(Left(s"\"$self\"")), Void.codec, origin)
     def toString: String = self
   def apply(name: String): Table = name
