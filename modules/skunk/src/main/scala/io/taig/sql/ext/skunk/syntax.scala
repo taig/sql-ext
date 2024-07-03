ffd8c9669d45db0561d5802760ca96efa10700ac
Niklas Klein <mail@taig.io>
2024-07-03 12:56:23 +0200

    Add codec +: and | extensions

---
+++ modules/skunk/src/main/scala/io/taig/sql/ext/skunk/syntax.scala
@@ -0,0 +1,27 @@
+package io.taig.sql.ext.skunk
+
+import skunk.Codec
+import cats.syntax.all.*
+
+object syntax:
+  extension [A](self: Codec[A])
+    def +:[B](codec: Codec[B]): Codec[Either[A, B]] =
+      (self.opt *: codec.opt).eimap {
+        case (Some(a), None) => a.asLeft.asRight
+        case (None, Some(b)) => b.asRight.asRight
+        case _               => "Invalid".asLeft
+      } {
+        case Left(a)  => (Some(a), None)
+        case Right(b) => (None, Some(b))
+      }
+
+  extension [A <: Matchable](self: Codec[A])
+    inline def |[B <: Matchable](codec: Codec[B]): Codec[A | B] =
+      (self.opt *: codec.opt).eimap {
+        case (Some(a), None) => a.asRight
+        case (None, Some(b)) => b.asRight
+        case _               => "Invalid".asLeft
+      } {
+        case a: A => (Some(a), None)
+        case b: B => (None, Some(b))
+      }
