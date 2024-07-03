a06a610a5b717bb53b3e11ffebbafe9a3935f01a
Niklas Klein <mail@taig.io>
2024-07-03 15:41:25 +0200

    Add Session alias Sx

--- modules/skunk/src/main/scala/io/taig/sql/ext/skunk/root.scala
+++ modules/skunk/src/main/scala/io/taig/sql/ext/skunk/root.scala
@@ -2,5 +2,8 @@ package io.taig.sql.ext.skunk
 
 import skunk.{Fragment, Void}
 import skunk.util.Origin
+import skunk.Session
+
+type Sx[F[_]] = Session[F]
 
 def fragment(sql: String)(using origin: Origin): Fragment[Void] = Fragment(List(Left(sql)), Void.codec, origin)
