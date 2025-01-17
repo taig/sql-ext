b39c34385aea134e868f04e77553d5b8cf266a73
Niklas Klein <mail@taig.io>
2025-01-17 09:53:53 +0100

    Fix missing sbt in CI

--- project/GitHubActionsGenerator.scala
+++ project/GitHubActionsGenerator.scala
@@ -13,6 +13,11 @@ object GitHubActionsGenerator {
       )
     )
 
+    val SetupSbt: Json = Json.obj(
+      "name" := "Setup sbt",
+      "uses" := "sbt/setup-sbt@v1"
+    )
+
     val Checkout: Json = Json.obj(
       "name" := "Checkout",
       "uses" := "actions/checkout@v4"
@@ -26,6 +31,7 @@ object GitHubActionsGenerator {
       "steps" := List(
         Step.Checkout,
         Step.SetupJava,
+        Step.SetupSbt,
         Json.obj(
           "name" := "Workflows",
           "run" := "sbt blowoutCheck"
@@ -48,6 +54,7 @@ object GitHubActionsGenerator {
       "steps" := List(
         Step.Checkout,
         Step.SetupJava,
+        Step.SetupSbt,
         Json.obj(
           "name" := "Release",
           "run" := "sbt ci-release",
